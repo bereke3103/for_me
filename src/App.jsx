be6fd9db94d69681a12643b56a6a35fd2468d9dc
Bereke Sakan <bereke3103@mail.ru>
2022-11-18 01:18:04 +0600

    without components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,71 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text: text,
+          completed: false,
+        },
+      ]);
+      setText('');
+    }
+  };
+
+  const removeTodo = (todoId) => {
+    const ElementsWhichSetTodos = todos.filter((todo) => todo.id !== todoId);
+
+    setTodos(ElementsWhichSetTodos);
+  };
+
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== todoId) return todo;
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <label>
+        <input
+          className="input_text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addTodo}>Add todo</button>
+      </label>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodoComplete(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <span className="delete" onClick={() => removeTodo(todo.id)}>
+              &times;
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
